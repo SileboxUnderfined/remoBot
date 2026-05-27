9c6c7c3199c8deea63317a212361296046804e3d
SileboxUnderfined <valerkafm@gmail.com>
2026-05-27 20:22:54 +0300

    added ALLOWED_IDS

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,14 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field, BeforeValidator
+from typing import Any, Annotated
+
+def parse_comma_separated_list(value: Any) -> list[int]:
+    if isinstance(value, str):
+        return [int(item.strip()) for item in value.split(",")]
+    return value
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
+    BOT_TOKEN: str = Field(...)
+    ALLOWED_IDS: Annotated[list[int], BeforeValidator(parse_comma_separated_list)] = Field(...)
+
+settings = Settings()
