42ad515d611228bcf3c68ffd672a19f91461cf42
SileboxUnderfined <valerkafm@gmail.com>
2026-05-29 13:33:31 +0300

    added tortoise initialization

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from .settings import settings
 from .modules.main_menu.dialogs import main_menu_dialog
 from .modules.main_menu.states import MainMenuSG
 from aiogram_dialog import setup_dialogs, DialogManager
+from tortoise import Tortoise, run_async
 
 storage = MemoryStorage()
 bot = Bot(token=settings.BOT_TOKEN,)
@@ -17,5 +18,12 @@ setup_dialogs(dp)
 async def start(message: Message, manager: DialogManager):
     await manager.start(MainMenuSG.start)
 
+async def main():
+    await Tortoise.init(
+        db_url='sqlite://db.sqlite3',
+        modules={'models':['models']}
+    )
+    await dp.start_polling(bot)
+
 if __name__ in "__main__":
-    dp.run_polling(bot)
+    run_async(main())
