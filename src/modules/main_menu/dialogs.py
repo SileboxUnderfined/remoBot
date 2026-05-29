6736ca457721e7f3bcfbc7414602788028126e16
SileboxUnderfined <valerkafm@gmail.com>
2026-05-29 11:40:11 +0300

    setup main menu module

---
+++ src/modules/main_menu/dialogs.py
@@ -0,0 +1,35 @@
+from aiogram_dialog import Window, Dialog
+from aiogram_dialog.widgets.text import Const, Multi
+from aiogram_dialog.widgets.kbd import Group, Next, Back, Button, SwitchTo, Start, Cancel, Column, Row
+from .states import MainMenuSG
+
+start_window_mainsg = Window(
+    Multi(
+        Const("Welcome"),
+    ),
+    Group(
+        SwitchTo(Const("Manage Hosts"),state=MainMenuSG.manage_hosts,id="manage_hosts_st"),
+        width=2
+    ),
+    state=MainMenuSG.start
+)
+
+manage_hosts_window_mainsg = Window(
+    Const("Manage Hosts"),
+    Column(
+        Group(
+            Start(Const("Add new Host"),id="add_new_host_st"),
+            Start(Const("Edit Host"),id="edit_host_st"),
+            Start(Const("Singular operations"),id="singular_operations_st"),
+            Start(Const("Multiple operations"),id="multiple_operations_st"),
+            width=2
+        ),
+        Back(Const("Back"))
+    ),
+    state=MainMenuSG.manage_hosts
+)
+
+main_menu_dialog = Dialog(
+    start_window_mainsg,
+    manage_hosts_window_mainsg
+)
---
+++ src/modules/main_menu/states.py
@@ -0,0 +1,5 @@
+from aiogram.filters.state import StatesGroup, State
+
+class MainMenuSG(StatesGroup):
+    start = State()
+    manage_hosts = State()
