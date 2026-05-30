8169d8d04d1b8b1b4d6ff9a0898aa6281e04f9b4
SileboxUnderfined <valerkafm@gmail.com>
2026-05-31 01:25:14 +0300

    Update host.py

--- src/models/host.py
+++ src/models/host.py
@@ -2,7 +2,7 @@ from tortoise.models import Model
 from tortoise.fields import IntField, CharField
 
 class Host(Model):
-    id = IntField(primary_key=True)
+    label = CharField(primary_key=True, max_length=255)
     hostname = CharField(max_length=255)
     username = CharField(max_length=255)
     password = CharField(max_length=255)
