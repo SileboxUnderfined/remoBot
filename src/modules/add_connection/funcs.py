3cac551e848600374f7ab6afd90e13c3946e08d3
SileboxUnderfined <valerkafm@gmail.com>
2026-05-31 01:20:14 +0300

    getter_confirm_data

--- src/modules/add_connection/funcs.py
+++ src/modules/add_connection/funcs.py
@@ -1,1 +1,12 @@
+from aiogram_dialog.widgets.input import TextInput
+from aiogram_dialog import DialogManager
+
+async def getter_confirm_data(dialog_manager: DialogManager, **kwargs):
+    return {
+        "ip_hostname": dialog_manager.dialog_data['ip_hostname'],
+        "port": dialog_manager.dialog_data['port'],
+        "username": dialog_manager.dialog_data['username'],
+        "password": dialog_manager.dialog_data['password']
+    }
+
 async def write_data(): pass # TODO: add
