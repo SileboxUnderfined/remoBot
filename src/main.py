3511297099a5d27941e5d22042a764c50a18e0d6
SileboxUnderfined <valerkafm@gmail.com>
2026-05-31 01:20:51 +0300

    fixed main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, User
 from aiogram.fsm.storage.memory import MemoryStorage
 from .settings import settings
 from .modules.main_menu.dialogs import main_menu_dialog
+from .modules.add_connection.dialogs import add_host_dialog
 from .modules.main_menu.states import MainMenuSG
 from aiogram_dialog import setup_dialogs, DialogManager
 from tortoise import Tortoise, run_async
+import asyncio, logging
 
 storage = MemoryStorage()
 bot = Bot(token=settings.BOT_TOKEN,)
 dp = Dispatcher(storage=storage)
 dp.include_router(main_menu_dialog)
+dp.include_router(add_host_dialog)
 setup_dialogs(dp)
 
 @dp.message(CommandStart())
-async def start(message: Message, manager: DialogManager):
-    await manager.start(MainMenuSG.start)
+async def start(message: Message, dialog_manager: DialogManager):
+    if message.from_user is None: return
+    
+    if message.from_user.id == settings.ALLOWED_IDS:
+        await dialog_manager.start(MainMenuSG.start)
 
 async def main():
     await Tortoise.init(
         db_url='sqlite://db.sqlite3',
-        modules={'models':['models']}
+        modules={'models':['src.models']}
     )
+    await Tortoise.generate_schemas(safe=True)
     await dp.start_polling(bot)
 
 if __name__ in "__main__":
-    run_async(main())
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
