69873331b271c5576a302c4d4ffcff4a01c9fca8
SileboxUnderfined <valerkafm@gmail.com>
2026-05-31 23:05:27 +0300

    verfy connection

--- src/modules/add_connection/funcs.py
+++ src/modules/add_connection/funcs.py
@@ -1,3 +1,4 @@
+from src.ssh.ssh_connection import SSHConnection, SSHOperationResult
 from aiogram_dialog.widgets.kbd import Button
 from aiogram.types import CallbackQuery
 from aiogram_dialog.widgets.input import TextInput
@@ -19,12 +20,20 @@ async def save_data_and_quit(callback: CallbackQuery, button: Button, manager: D
         await callback.answer("This host label already exists!")
         return
 
-    await Host.create(
+    host = Host(
         label=manager.dialog_data['label'],
-        hostname=manager.dialog_data['ip_hostname']+manager.dialog_data['port'],
+        hostname=manager.dialog_data['ip_hostname'],
+        port=manager.dialog_data['port'],
         username=manager.dialog_data['username'],
         password=manager.dialog_data['password'],
     )
 
+    async with SSHConnection(host) as conn:
+        conn_result: SSHOperationResult = await conn.check_connection()
+
+    if not conn_result.success:
+        await callback.answer(f"Failed while checking connection: {conn_result.result}")
+
+    await host.save()
     await callback.answer("Host successfully added!")
     await manager.start(MainMenuSG.start, mode=StartMode.RESET_STACK)
